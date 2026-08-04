5ae9a893cf76a33ed207cebb786ce9ae3c4e7db0
oleeriksen <oleeriksen1958@gmail.com>
2026-08-04 11:30:42 +0200

    Initial commit

---
+++ Shared/Paths.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Shared
+{
+    public class Paths
+    {
+        public static readonly string SQLITE_DATABASE = @"/Users/oler/Data/searchDBmedium.db";
+        
+        // set correct password
+        public static readonly string POSTGRES_DATABASE = "Server=127.0.0.1:5432;User Id=oler;Password=XXXX;database=search";
+    }
+}

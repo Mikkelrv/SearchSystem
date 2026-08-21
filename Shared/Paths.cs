dd8d7ed7d2b52f27b7b37c37a6371caed4e4cd71
oleeriksen <oleeriksen1958@gmail.com>
2026-08-21 13:19:42 +0200

    div

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -3,9 +3,9 @@ namespace Shared
 {
     public class Paths
     {
-        public static readonly string SQLITE_DATABASE = @"/Users/oler/Documents/Data/searchDBsmall.db";
+        public static readonly string SQLITE_DATABASE = @"/Users/oler/Documents/Data/searchDBmedium.db";
         
         // set correct password
-        public static readonly string POSTGRES_DATABASE = "Server=127.0.0.1:5432;User Id=oler;Password=1234;database=searchsmall";
+        public static readonly string POSTGRES_DATABASE = "Server=127.0.0.1:5432;User Id=oler;Password=XXXX;database=searchmedium";
     }
 }
